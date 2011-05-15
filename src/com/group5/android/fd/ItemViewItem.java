0557456838b7fc3615da16ea925e7f7ae64bd056
Dao Hoang Son <daohoangson@gmail.com>
2011-05-15 12:27:27 +0700

    clean up

--- src/com/group5/android/fd/ItemViewItem.java
+++
@@ -1,9 +0,0 @@
-package com.group5.android.fd;
-
-import android.content.Context;
-
-public class ItemViewItem extends ItemView{
-	public ItemViewItem ( Context context, String text){
-		super ( context,text);
-	}
-}
--- src/com/group5/android/fd/ItemViewTable.java
+++
@@ -1,9 +0,0 @@
-package com.group5.android.fd;
-
-import android.content.Context;
-
-public class ItemViewTable extends ItemView{
-	public ItemViewTable ( Context context, String text){
-		super ( context,text);
-	}
-}
