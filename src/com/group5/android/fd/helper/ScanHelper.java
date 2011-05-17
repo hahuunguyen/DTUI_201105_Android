16846506d0c7778a4483dabad44171d412bd696b
Dao Hoang Son <daohoangson@gmail.com>
2011-05-17 19:38:02 +0700

    fix missing file before moving to Ubuntu

---
+++ src/com/group5/android/fd/helper/ScanHelper.java
@@ -0,0 +1,37 @@
+package com.group5.android.fd.helper;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.group5.android.fd.entity.AbstractEntity;
+import com.group5.android.fd.entity.TableEntity;
+
+public class ScanHelper {
+	public static AbstractEntity parseScannedContents(String contents) {
+		AbstractEntity entity = null;
+
+		try {
+			JSONObject jsonObject = new JSONObject(contents);
+			JSONArray names = jsonObject.names();
+
+			for (int i = 0; i < names.length(); i++) {
+				String name = names.getString(i);
+				if (name.equals("table")) {
+					TableEntity table = new TableEntity();
+					table.parse(jsonObject.getJSONObject(name));
+
+					return table;
+				}
+			}
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return entity;
+	}
+}
